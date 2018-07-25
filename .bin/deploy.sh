51b2e602dc42654920d03ec42f75cb84f14b16df
Chris Ruppel <chris@humanitarianresponse.info>
2018-07-25 14:04:12 +0200

    :bug: ensure deploy script has a local site to run against
    The UnCSS task needs a locally-running copy of the site to
    compute the critical styles. The deploy script now runs the
    browser-sync task in the background while doing the other
    tasks necessary to prep the assets.

--- .bin/deploy.sh
+++ .bin/deploy.sh
@@ -7,9 +7,20 @@
 # configure node/gulp for deploy
 export NODE_ENV='production'
 
+# run server in background so we can complete UnCSS task during `gulp deploy`
+gulp dev:bs > /dev/null &
+
 # build assets
 gulp deploy
 git add assets
+
+# terminate `gulp dev:bs` background task
+#
+# In the context of this shell script, we can kill the last background job we
+# started because the parent shell doesn't apply in here.
+#
+# @see https://stackoverflow.com/a/30171578/175551
+kill %-
 
 # instruct developer to review/commit/push
 echo ""
